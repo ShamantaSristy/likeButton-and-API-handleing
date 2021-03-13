cebcd711a718a8190d3ab29905ab6fcdaf41ef40
Shamanta Sristy <shamantasristy@gmail.com>
2021-03-13 20:57:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [likeColor, setLikeColor] = useState('');
+  // const handleLike = () => {
+  //   ;
+  // }
+  const [user, setUser] = useState([]);
+const [singleUser, setSingleUser] = useState({});
+const [randomUser, setRandomUser] = useState({});
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUser(data))
+
+      // single user
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.json())
+    .then(data => setSingleUser(data));
+
+    // random user
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => setRandomUser(data.results[0]));
+  
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThumbUpAltIcon onClick={() => setLikeColor( likeColor ? '' : 'primary')} color={likeColor}></ThumbUpAltIcon>
+      <h1>Name: {singleUser.name}</h1>
+      <h2>Random Gender: {randomUser.gender}</h2>
+      <h2>Random Name: {randomUser.name?.first}</h2>
+      {
+        user.map(user => <li>{user.name}</li>)
+      }
     </div>
   );
 }
